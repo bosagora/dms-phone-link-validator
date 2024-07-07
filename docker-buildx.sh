e51be7c532c2418b8af1303c1c8e10e08abd0491
Danial <win303065@gmail.com>
2024-07-07 17:19:15 +0900

    Change docker image name

--- docker-buildx.sh
+++ docker-buildx.sh
@@ -3,6 +3,6 @@
 TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/phone-link-validator:"$TAG_NANE" -f Dockerfile --push .
-docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/phone-link-validator:latest -f Dockerfile --push .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/acc-phone-link-validator:"$TAG_NANE" -f Dockerfile --push .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/acc-phone-link-validator:latest -f Dockerfile --push .
 
